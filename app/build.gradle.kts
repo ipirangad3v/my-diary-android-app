ab7f251ae321ab6354cadbf57d96b6cbeadb42d8
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2025-09-12 22:49:16 -0300

    feat: update app version to 1.9.0 and change launcher background color

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "digital.tonima.mydiary"
     minSdk = 26
     targetSdk = 36
-    versionCode = 9
-    versionName = "1.8.0"
+    versionCode = 10
+    versionName = "1.9.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
