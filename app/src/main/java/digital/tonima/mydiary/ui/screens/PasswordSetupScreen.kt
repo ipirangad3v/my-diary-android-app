b58fb09a94c83e000760a9383527d48bc97b7696
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2025-09-15 23:22:01 -0300

    feat: enhance input handling; add keyboard options and focus management for password setup and entry screens

--- app/src/main/java/digital/tonima/mydiary/ui/screens/PasswordSetupScreen.kt
+++ app/src/main/java/digital/tonima/mydiary/ui/screens/PasswordSetupScreen.kt
@@ -1,13 +1,15 @@
 package digital.tonima.mydiary.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -19,9 +21,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction.Companion.Done
 import androidx.compose.ui.text.input.ImeAction.Companion.Next
+import androidx.compose.ui.text.input.KeyboardType.Companion.Password
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -51,14 +57,19 @@ fun PasswordSetupScreen(
             }
         }
     }
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val scrollState = rememberScrollState()
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+            .padding(16.dp)
+            .verticalScroll(scrollState),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.weight(1f))
+
         Text(text = stringResource(create_master_password), style = MaterialTheme.typography.headlineSmall)
         Spacer(modifier = Modifier.height(8.dp))
         Text(text = stringResource(master_password_description), style = MaterialTheme.typography.bodyMedium)
@@ -71,7 +82,15 @@ fun PasswordSetupScreen(
             visualTransformation = PasswordVisualTransformation(),
             isError = uiState.errorResId != null,
             modifier = Modifier.fillMaxWidth(),
-            keyboardActions = KeyboardActions { Next }
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = Password,
+                imeAction = Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = {
+                    focusManager.moveFocus(FocusDirection.Down)
+                })
         )
         Spacer(modifier = Modifier.height(8.dp))
 
@@ -91,8 +110,18 @@ fun PasswordSetupScreen(
             label = { Text(stringResource(confirm_password)) },
             visualTransformation = PasswordVisualTransformation(),
             isError = uiState.errorResId != null,
+            singleLine = true,
             modifier = Modifier.fillMaxWidth(),
-            keyboardActions = KeyboardActions { Done }
+            keyboardOptions = KeyboardOptions(
+                keyboardType = Password,
+                imeAction = Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                    keyboardController?.hide()
+                }
+            )
         )
 
         uiState.errorResId?.let { errorRes ->
@@ -111,5 +140,6 @@ fun PasswordSetupScreen(
         ) {
             Text(stringResource(save_password))
         }
+        Spacer(modifier = Modifier.weight(1f))
     }
 }
